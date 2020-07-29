34a0a9ffe65b5c5be404c5055015d3d63fbf8286
Brian Gaines <briang.sandiego@gmail.com>
2020-07-28 18:50:41 -0700

    fixed data loading issue, set viewport dims for cypress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDataFetch } from './hooks/';
 import {
   DATASET_BASELINE_URL,
   DATASET_1,
-  DATASET_2,
   DASHBOARD_TITLE,
   DASHBOARD_DESC,
 } from './common/constants';
@@ -12,36 +11,27 @@ import styled from 'styled-components';
 import './theme/nautical.css';
 
 const App = () => {
-  const [
-    { data: response, isLoading, isError, error, url },
-    setUrl,
-  ] = useDataFetch(DATASET_1.url, []);
+  const [{ data: response, isLoading, isError, error, url }, setUrl] = useDataFetch(
+    DATASET_1.url,
+    []
+  );
 
   const dataSet = response?.data;
 
   const onReload = (e, dataFile) => {
     e.preventDefault();
-    console.log('onReload', dataSet, url, `${DATASET_BASELINE_URL}${dataFile}`);
-
     setUrl(`${DATASET_BASELINE_URL}${dataFile}`);
-
-    // setUrl(dataFile === DATASET_1.onClickArg ? DATASET_1.url : DATASET_2.url);
   };
 
-  useEffect(() => {
-    console.log('useEffect', dataSet, url);
-  });
-
-  console.log({ response, dataSet, isLoading, isError, error, url });
   return (
     <Container>
       <Header heading={DASHBOARD_TITLE} description={DASHBOARD_DESC} />
-      {dataSet && <Body key={url} data={dataSet} onReload={onReload} />}
+      {dataSet && !isLoading && <Body key={url} data={dataSet} onReload={onReload} />}
     </Container>
   );
 };
 
-export default App;
+export default React.memo(App);
 
 const Container = styled.div`
   background-color: var(--secondarybg);
--- src/hooks/useStatistics.jsx
+++ src/hooks/useStatistics.jsx
@@ -3,19 +3,7 @@ import { useNumArray } from './';
 
 export const useStatistics = (initialDataSet = []) => {
   const [dataSet, setDataSet] = useState(initialDataSet);
-
-  console.log('useStatistics', dataSet);
-
   const [mean, median, stdDev, mode] = useNumArray(dataSet);
-  console.log('useStatistics', {
-    dataSet,
-    setDataSet,
-    mean,
-    median,
-    stdDev,
-    mode,
-  });
-
   return { dataSet, setDataSet, mean, median, stdDev, mode };
 };
 

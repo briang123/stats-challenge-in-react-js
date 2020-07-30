1c1a1a17e21357dd1e2ff87aa9092030ddee3b18
Brian Gaines <briang.sandiego@gmail.com>
2020-07-29 18:22:04 -0700

    updated Body tests

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -6,21 +6,13 @@ import styled from 'styled-components';
 
 export const Body = ({ data, onReload }) => {
   if (!onReload) console.error('Missing onReload event handler');
-  const { dataSet, mean, median, mode, setDataSet, stdDev } = useStatistics(
-    data
-  );
-
+  const { dataSet, mean, median, mode, setDataSet, stdDev } = useStatistics(data);
+  const isValidReloadFunction = onReload && typeof onReload === 'function';
   return (
     <BodyContainer data-testid="body-container">
-      <Dashboard
-        data-testid="dashboard"
-        mean={mean}
-        median={median}
-        stdDev={stdDev}
-        mode={mode}
-      />
+      <Dashboard data-testid="dashboard" mean={mean} median={median} stdDev={stdDev} mode={mode} />
       <Form data-testid="form" dataSet={dataSet} setDataSet={setDataSet} />
-      {onReload && <DataReload data-testid="data-reload" onClick={onReload} />}
+      {isValidReloadFunction && <DataReload data-testid="data-reload" onClick={onReload} />}
     </BodyContainer>
   );
 };
--- src/components/Body/__tests__/Body.test.js
+++ src/components/Body/__tests__/Body.test.js
@@ -1,104 +1,118 @@
 import React from 'react';
 import { cleanup, render, fireEvent } from '@testing-library/react';
-import { create } from 'react-test-renderer';
-import Body from '../Body';
 import { DATASET_1 } from './../../../common/constants';
+import Body from '../Body';
 
 afterEach(() => {
   cleanup();
   console.error.mockClear();
+  mockFn.mockClear();
 });
 
 console.error = jest.fn();
+const mockFn = jest.fn();
+
+const TILE_VALUE_MEAN_ID = 'tile-value-Mean';
+const TILE_VALUE_MEDIAN_ID = 'tile-value-Median';
+const TILE_VALUE_STDDEV_ID = 'tile-value-Std Deviation';
+const TILE_VALUE_MODE_ID = 'tile-value-Mode';
 
 describe('<Body />', () => {
-  it('Body component without props should log an error, but show dashboard without the data', () => {
-    const { queryByText, getByTestId } = render(<Body />);
+  it('Body component without props should log an error', () => {
+    render(<Body />);
     expect(console.error).toHaveBeenCalled();
+  });
 
-    const meanValue = getByTestId('tile-value-Mean');
+  it('Body component without props should show dashboard without the data', () => {
+    const { getByTestId } = render(<Body />);
+
+    const meanValue = getByTestId(TILE_VALUE_MEAN_ID);
     expect(meanValue.textContent).toBe('');
 
-    const medianValue = getByTestId('tile-value-Median');
+    const medianValue = getByTestId(TILE_VALUE_MEDIAN_ID);
     expect(medianValue.textContent).toBe('');
 
-    const stdDevValue = getByTestId('tile-value-Std Deviation');
+    const stdDevValue = getByTestId(TILE_VALUE_STDDEV_ID);
     expect(stdDevValue.textContent).toBe('');
 
-    const modeValue = getByTestId('tile-value-Mode');
+    const modeValue = getByTestId(TILE_VALUE_MODE_ID);
     expect(modeValue.textContent).toBe('');
+  });
+
+  it('Body component with only data prop and no reload function prop should log an error', () => {
+    render(<Body data={[1, 2]} />);
+    expect(console.error).toHaveBeenCalled();
+  });
+
+  it('Body component with only data prop and no reload function prop should show dashboard without the data', () => {
+    const { getByTestId } = render(<Body data={[1, 2]} />);
+
+    const meanValue = getByTestId(TILE_VALUE_MEAN_ID);
+    expect(meanValue.textContent).toBe('1.500000');
+
+    const medianValue = getByTestId(TILE_VALUE_MEDIAN_ID);
+    expect(medianValue.textContent).toBe('1.500000');
+
+    const stdDevValue = getByTestId(TILE_VALUE_STDDEV_ID);
+    expect(stdDevValue.textContent).toBe('0.707107');
+
+    const modeValue = getByTestId(TILE_VALUE_MODE_ID);
+    expect(modeValue.textContent).toBe('1.000000,2.000000');
+  });
+
+  it('Body component without onReload prop should hide the reload buttons', () => {
+    const { queryByText } = render(<Body />);
 
     const reloadBtn1234 = queryByText(DATASET_1.buttonText);
     expect(reloadBtn1234).toBeNull();
   });
 
-  it('Body component with only data prop and no reload function prop should log an error, but show dashboard without the data', () => {
-    const { getByTestId } = render(<Body data={[1, 2]} />);
-    expect(console.error).toHaveBeenCalled();
-
-    const meanValue = getByTestId('tile-value-Mean');
-    expect(meanValue.textContent).toBe('1.500000');
-
-    const medianValue = getByTestId('tile-value-Median');
-    expect(medianValue.textContent).toBe('1.500000');
-
-    const stdDevValue = getByTestId('tile-value-Std Deviation');
-    expect(stdDevValue.textContent).toBe('0.707107');
-
-    const modeValue = getByTestId('tile-value-Mode');
-    expect(modeValue.textContent).toBe('1.000000,2.000000');
+  it('Body component with non-function passed in as onReload prop should not render the reload buttons ', () => {
+    const { queryByText } = render(<Body data={[1, 2]} onReload="" />);
+    const reloadBtn1234 = queryByText(DATASET_1.buttonText);
+    expect(reloadBtn1234).toBeNull();
   });
 
-  //TODO: NEED MORE TESTS....
+  it('Should render body when passing valid props', () => {
+    const { getByTestId } = render(<Body data={[1, 2]} onReload={mockFn} />);
 
-  // it('Should match snapshot without heading text', () => {
-  //   const tree = create(<Header />).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+    const bodyContainer = getByTestId('body-container');
+    expect(bodyContainer.tagName).toBe('DIV');
+    expect(bodyContainer.childElementCount).toBe(3);
+  });
 
-  // it('Should render logo and heading text', () => {
-  //   const { getByTestId } = render(<Header heading="foo" />);
+  it('Should render form when passing valid props', () => {
+    const { getByTestId } = render(<Body data={[1, 2]} onReload={mockFn} />);
 
-  //   const container = getByTestId('header');
-  //   expect(container.tagName).toBe('DIV');
-  //   expect(container.childElementCount).toBe(2);
+    const formContainer = getByTestId('form-container');
+    expect(formContainer.tagName).toBe('DIV');
+    expect(formContainer.childElementCount).toBe(1);
 
-  //   const logo = container.firstChild;
-  //   expect(logo.tagName).toBe('IMG');
+    const inputBtnGroupContainer = formContainer.firstChild;
+    expect(inputBtnGroupContainer.childElementCount).toBe(2);
 
-  //   const header = container.lastChild;
-  //   expect(header.tagName).toBe('P');
-  //   expect(header.textContent).toBe('foo');
-  // });
+    expect(inputBtnGroupContainer.firstChild.tagName).toBe('INPUT');
+    expect(inputBtnGroupContainer.lastChild.tagName).toBe('BUTTON');
+  });
 
-  // it('Should match snapshot with heading text', () => {
-  //   const tree = create(<Header heading="foo" />).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('Should render reload container when passing valid props', () => {
+    const { getByTestId } = render(<Body data={[1, 2]} onReload={mockFn} />);
 
-  // it('Should render logo, heading text, and description', () => {
-  //   const { getByTestId, getByAltText } = render(
-  //     <Header heading="foo" description="bar" />
-  //   );
+    const reloadContainer = getByTestId('reload-container');
+    expect(reloadContainer.tagName).toBe('DIV');
+    expect(reloadContainer.childElementCount).toBe(1);
 
-  //   const container = getByTestId('header');
-  //   expect(container.tagName).toBe('DIV');
-  //   expect(container.childElementCount).toBe(3);
+    const reloadButtonContainer = reloadContainer.firstChild;
+    expect(reloadButtonContainer.childElementCount).toBe(2);
+    expect(reloadButtonContainer.firstChild.tagName).toBe('BUTTON');
+    expect(reloadButtonContainer.lastChild.tagName).toBe('BUTTON');
+  });
 
-  //   const logo = getByAltText('logo');
-  //   expect(logo.tagName).toBe('IMG');
+  it('Reload button should trigger callback when clicked and passing valid onReload function as prop', () => {
+    const { queryByText } = render(<Body data={[1, 2]} onReload={mockFn} />);
 
-  //   const header = getByTestId('heading');
-  //   expect(header.tagName).toBe('P');
-  //   expect(header.textContent).toBe('foo');
-
-  //   const description = getByTestId('description');
-  //   expect(description.tagName).toBe('DIV');
-  //   expect(description.textContent).toBe('bar');
-  // });
-
-  // it('Should match snapshot with heading and description text', () => {
-  //   const tree = create(<Header heading="foo" descriptions="bar" />).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+    const reloadBtn1234 = queryByText(DATASET_1.buttonText);
+    fireEvent.click(reloadBtn1234);
+    expect(console.error).not.toHaveBeenCalled();
+  });
 });
